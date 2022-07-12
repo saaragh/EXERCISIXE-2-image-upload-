18c914c65a527fa4d8dfb45f12a57cd8412c2eaa
niteshkamal001 <106018618+niteshkamal001@users.noreply.github.com>
2022-07-12 05:06:44 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+from PIL import Image 
 from fastapi import FastAPI, File, UploadFile
 import shutil
 from typing import List
+import os
+from fastapi.responses import FileResponse
+
 
 app = FastAPI()
 
@@ -11,8 +15,19 @@ async def image(image: UploadFile = File(...)):
     
     return {"filename": image.filename}
 
+
+@app.post("/grey_image/")
+async def create_grey_img(file:UploadFile):
+    filename= await get_image(file)
+    img = Image.open(filename)
+    img.convert('L').save('grey_img.jpg')
+    return FileResponse("grey_img.jpg")
+
+
 @app.post("/multiimage")
 async def image(images: List[UploadFile] = File(...)):
     for image in images:
         with open(image.filename, "wb") as buffer:
             shutil.copyfileobj(image.file, buffer)
+
+        return {"filename": image.filename}
